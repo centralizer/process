11d0cad6718a94e77a160d4179dd5ec23ca94b85
Aaron Balthaser <aaronbalthaser@gmail.com>
2024-01-05 02:00:07 -0800

    dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,24 @@
 import { HttpModule } from "@nestjs/axios";
 import { BullModule } from "@nestjs/bull";
 import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 
 import { YoutubeDownloadConsumer } from "./queues/consumers/youtube-download.consumer";
 import { YoutubeDownloadProducer } from "./queues/producers/youtube-download.producer";
 
+const env = dotenv.config({ path: path.resolve(`.env.${process.env.NODE_ENV}`) });
+
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`,
-    }),
-    BullModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        redis: {
-          host: configService.get("REDIS_HOST"),
-          port: configService.get("REDIS_PORT"),
-          password: configService.get("REDIS_PASS"),
-        },
-      }),
+    BullModule.forRoot({
+      redis: {
+        host: env.parsed.REDIS_HOST,
+        port: +env.parsed.REDIS_PORT,
+        password: env.parsed.REDIS_PASS,
+      }
     }),
     BullModule.registerQueue(
       {
